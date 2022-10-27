b8715aa13461e8355dd4a94e1a26846d95293562
Rogério Alonso <69876702+rogerioloualonso@users.noreply.github.com>
2022-10-27 19:29:33 -0300

    Corrigindo responsividade

--- js/main.js
+++ js/main.js
@@ -3,6 +3,27 @@
  *
  * ------------------------------------------------------------------- */
 
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 10) { // 140 é a distancia que vc rola antes da logo sumir
+      $("#header_index").fadeOut();
+      $("#header_musica").fadeOut();
+      $("#header_about").fadeOut();
+      $("#header_contact").fadeOut();
+      $("#header_cristo").fadeOut();
+      $("#header_colonizacao").fadeOut();
+      $("#header_arquitetura").fadeOut();
+    } else {
+      $("#header_index").fadeIn();
+      $("#header_musica").fadeIn();
+      $("#header_about").fadeIn();
+      $("#header_contact").fadeIn();
+      $("#header_cristo").fadeIn();
+      $("#header_colonizacao").fadeIn();
+      $("#header_arquitetura").fadeIn();
+    }
+  });
+
 (function($) {
 
     "use strict";
