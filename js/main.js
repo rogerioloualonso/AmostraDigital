b652aaf8716d57a2ecff07698bff1ed52305a077
Rogério Alonso <69876702+rogerioloualonso@users.noreply.github.com>
2022-10-27 23:41:59 -0300

    Consertando bug 4

--- js/main.js
+++ js/main.js
@@ -2,7 +2,27 @@
  * TypeRite - Main JS
  *
  * ------------------------------------------------------------------- */
-
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 10) {
+      $("#header_index").fadeOut();
+      $("#header_musica").fadeOut();
+      $("#header_about").fadeOut();
+      $("#header_contact").fadeOut();
+      $("#header_cristo").fadeOut();
+      $("#header_colonizacao").fadeOut();
+      $("#header_arquitetura").fadeOut();
+    } else {
+      $("#header_index").fadeIn();
+      $("#header_musica").fadeIn();
+      $("#header_about").fadeIn();
+      $("#header_contact").fadeIn();
+      $("#header_cristo").fadeIn();
+      $("#header_colonizacao").fadeIn();
+      $("#header_arquitetura").fadeIn();
+    }
+  });
+  
 (function($) {
 
     "use strict";
